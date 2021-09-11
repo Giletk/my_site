916bfc1494bee7e0ab89c6fa690451d04bcc3413
Myself <giletk@yandex.ru>
2021-09-11 19:40:00 +0300

    first  one

---
+++ taskmanager/registration/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+
+def index(request):
+    form = UserCreationForm()
+    return render(request, 'registration/register.html', {"Title": "Reg", "form": form})
